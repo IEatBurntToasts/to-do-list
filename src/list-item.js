7fbe06c78af01c50906e61e9831fd20b6776b12e
Quan <61860115+Dracozure@users.noreply.github.com>
2024-12-22 19:59:43 -0800

    Make list item module

---
+++ src/list-item.js
@@ -0,0 +1,36 @@
+const listItem = (title,description='',dueDate='',priority='',note='') {
+    return {
+        get title() {
+            return title;
+        },
+        set title(newTitle) {
+            title = newTitle;
+        },
+        get description() {
+            return description;
+        },
+        set description(newDescription) {
+            description = newDescription;
+        },
+        get dueDate() {
+            return dueDate;
+        },
+        set dueDate(newDueDate) {
+            dueDate = newDueDate;
+        },
+        get priority() {
+            return priority;
+        },
+        set priority(newPriority) {
+            priority = newPriority;
+        },
+        get note() {
+            return note;
+        },
+        set note(newNote) {
+            note = newNote;
+        }
+    }
+}
+
+export { listItem };
